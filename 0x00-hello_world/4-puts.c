a6c5141fddfc3f209893bc9fda7c12ecc18ab13b
kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-02-16 16:02:06 +0100

    Write a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char str [] = " \"Programming is like building a multilingual puzzle ";
+	char str [] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 
 	return (0);
