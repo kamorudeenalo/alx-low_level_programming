ab47c1b9a645a127bf7ad82fa5090522981f99a6
kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-02-16 16:00:52 +0100

    Write a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Prints string to the standard output
+  *
+  *Return: 0 (Success)
+  */
+
+int main(void)
+{
+	char str [] = " \"Programming is like building a multilingual puzzle ";
+	puts(str);
+
+	return (0);
+}
