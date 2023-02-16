67311ead3d9d87496ca4bd3cee28ae250c95d0cd
kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-02-16 21:16:23 +0100

    Write a script that generates the assembly code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+i#include <stdio.h>
+
+/**
+  * main - Prints sizes of various data types
+  *
+  *Return: Always return 0 (Success)
+  */
+
+int main(void)
+{
+	prinf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+
+	return (0);
+}
