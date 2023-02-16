b86d77e95b9f0d200871a78bbda5ac5141c23b3e
kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-02-16 16:10:23 +0100

    Write a C program that prints a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Prints string to the standard output
+  *
+  * Return: Always return 0 (Success)
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
