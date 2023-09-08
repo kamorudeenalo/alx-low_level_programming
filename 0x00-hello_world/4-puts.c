45d642baf052f023018aef2d4e5fba2345afc149
kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-09-07 20:07:40 -0700

    Put out the message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
