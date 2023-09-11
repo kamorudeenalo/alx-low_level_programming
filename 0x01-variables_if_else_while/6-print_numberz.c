0f0bceb48e8fae189b313ca275ddccc7093d3ff5
kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-09-11 14:35:38 -0700

    Print base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 < 10; base10++)
+	{
+		printf('0' + base10);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
