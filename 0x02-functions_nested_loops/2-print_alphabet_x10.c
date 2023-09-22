52eafb40b4fb25358243a7672221aee03d6ad455
Kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-09-22 00:01:09 +0000

    Print alpha 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Prints the lowercase ten times.
+  *
+  * Return: nothing to the stdout.
+  */
+
+void print_alphabet_x10(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 0; num < 9; num++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
