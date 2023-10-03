4c668b3ede12731456e916dd947ada164b56316a
Kamorudeenalo <kamorudeenalowonle@gmail.com>
2023-10-03 01:05:20 +0000

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: is a parameter
+ *
+ * Return: the value last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
